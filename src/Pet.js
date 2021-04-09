98493422b2d4debb74add928ab9e2bd2bfe62f84
Nivethida <nivethida@Aruns-MacBook-Pro-2.local>
2021-04-09 09:09:53 -0700

    async and routing

--- src/Pet.js
+++ src/Pet.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-export default function Pet({name, animal, breed}) {
-  //return React.createElement("div", {}, [
-    //React.createElement("h1", {}, name),
-    //React.createElement("h2", {}, animal),
-    //React.createElement("h3", {}, breed),
- // ]);
- return (
-     <div>
-         <h1>{name}</h1>
-         <h1>{animal}</h1>
-         <h1>{breed}</h1>
-     </div>
- )
+export default function Pet({name, animal, breed, media, location, id}) {
+    let hero = "http://placecorgi.com/300/300";
+    if(media.length){
+        hero = media[0].small;
+    }
+    return(
+        <a href={`/details/${id}`} className="pet">
+            <div className="image-container">
+                <img src={hero} alt={name}></img>
+            </div>
+            <div className="info">
+                <h1>{name}</h1>
+                <h2>{`${animal}-${breed}-${location}`}</h2>
+            </div>
+        </a>
+    )
 };
